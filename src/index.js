15f627c20482e117c30a99829a76a1fc31d1eb8d
Austin Wise <austin@wisebydesign.io>
2019-01-23 19:29:45 -0500

    Fix Index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import { render } from "react-dom";
+//render is what is shown in the view. 'react-dom' signifies that
+//we are rendering it TO THE DOM
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//rather than these, we can render out the Router tag, as they are indirectly used there...
+// import StorePicker from "./components/StorePicker.js";
+// import App from "./components/App.js";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//bring in CSS for the entire application
+/*because we are set up for create-react-app, 
+Webpack config is smart enough to know this is not a .js file, 
+so its going to load in the css and stick it into a style tag,
+where it can be hotloaded as it changes.
+*/
+import Router from "./components/Router";
+import "./css/style.css";
+
+render(<Router />, document.querySelector("#main"));
